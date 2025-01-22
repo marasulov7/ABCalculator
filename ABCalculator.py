3660b638fb771f5edd7a20872ab5d19bb82dd070
marasulov7 <marasulovalish@gmail.com>
2025-01-22 15:09:13 +0500

    Создание файла программы калькулятора

---
+++ ABCalculator.py
@@ -0,0 +1,11 @@
+#  A/B Calculator
+import tkinter as tk
+
+# Создание главного окна
+root=tk.Tk()
+root.geometry("280x300")
+root.title("A/B Calculator")
+
+# Запуск цикла mainloop
+root.mainloop()
+
