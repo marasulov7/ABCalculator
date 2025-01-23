1f1eaf9cdaa6c1ec1cc51be0ec3b8c5534458fbb
marasulov7 <marasulovalish@gmail.com>
2025-01-23 15:58:04 +0500

    Добавление меток и полей ввода

--- ABCalculator.py
+++ ABCalculator.py
@@ -14,6 +14,40 @@ root.title("A/B Calculator")
 lbl_title = tk.Label(text="А/В Калькулятор", font=('Helvetica', 16, 'bold'), fg='#0000cc') #fg-цвет текста
 lbl_title.place(x=55, y=20)
 
+# Добавление метки заголовка контрольной группы
+lbl_title1 = tk.Label(text="Контрольная группа", font=('Helvetica', 12, 'bold'), fg='#0066ff') #fg-цвет текста
+lbl_title1.place(x=25, y=55)
+
+# Добавление полей ввода контрольной группы
+lbl_visitors1 = tk.Label(text="Посетители", font=('Helvetica', 10, 'bold')) 
+lbl_visitors1.place(x=25, y=85)
+
+ent_visitors1 = tk.Entry(font=('Helvetica', 10, 'bold')) 
+ent_visitors1.place(x=115, y=85, width=90, height=20)
+
+lbl_conversions1 = tk.Label(text="Конверсии", font=('Helvetica', 10, 'bold')) 
+lbl_conversions1.place(x=25, y=115)
+
+ent_conversions1 = tk.Entry(font=('Helvetica', 10, 'bold')) 
+ent_conversions1.place(x=115, y=115, width=90, height=20)
+
+# Добавление метки заголовка тестовой группы
+lbl_title2 = tk.Label(text="Тестовая группа", font=('Helvetica', 12, 'bold'), fg='#008800') #fg-цвет текста
+lbl_title2.place(x=25, y=145)
+
+# Добавление полей ввода тестовой группы
+lbl_visitors2 = tk.Label(text="Посетители", font=('Helvetica', 10, 'bold')) 
+lbl_visitors2.place(x=25, y=175)
+
+ent_visitors2 = tk.Entry(font=('Helvetica', 10, 'bold')) 
+ent_visitors2.place(x=115, y=175, width=90, height=20)
+
+lbl_conversions2 = tk.Label(text="Конверсии", font=('Helvetica', 10, 'bold')) 
+lbl_conversions2.place(x=25, y=205)
+
+ent_conversions2 = tk.Entry(font=('Helvetica', 10, 'bold')) 
+ent_conversions2.place(x=115, y=205, width=90, height=20)
+
 # Добавление кнопки "Рассчитать"
 btn_process = tk.Button(root, text="Рассчитать", font = ('Helvetica', 10, 'bold'))
 btn_process.place(x=25, y=250, width=90, height=30)
